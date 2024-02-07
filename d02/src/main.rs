3327f9fae3916c1b565e71fe8bf4e7a02ab99b2f
Josh Pilkington <j.pilkington@hotmail.com>
2024-02-07 12:59:57 +0000

    Part 1 done

--- d02/src/main.rs
+++ d02/src/main.rs
@@ -1,61 +1,62 @@
-use std::fs;
+use std::{fs, str::FromStr};
 
+#[derive(Debug)]
 struct Round {
     red: i32,
     green: i32,
     blue: i32,
 }
 
-impl Round {
-    fn new(round_str: &str) -> Self {
-        let mut green: i32 = 0;
-        let mut red: i32 = 0;
-        let mut blue: i32 = 0;
-        round_str
-            .split(",")
-            .into_iter()
-            .map(|command| match command {
-                command if command.ends_with("red") => {
-                    red = command
-                        .split_whitespace()
-                        .next()
-                        .unwrap()
-                        .trim()
-                        .parse::<i32>()
-                        .expect("Number not found")
-                }
-                command if command.ends_with("blue") => {
-                    blue = command
-                        .split_whitespace()
-                        .next()
-                        .unwrap()
-                        .trim()
-                        .parse::<i32>()
-                        .expect("Number not found")
-                }
-                command if command.ends_with("green") => {
-                    green = command
-                        .split_whitespace()
-                        .next()
-                        .unwrap()
-                        .trim()
-                        .parse::<i32>()
-                        .expect("Number not found")
-                }
-                _ => (),
-            });
+#[derive(Debug)]
+enum Error {
+    NoColourFound,
+}
 
-        Self {
-            red: red,
-            blue: blue,
-            green: green,
+const MAX_ROUND: Round = Round {
+    red: 12,
+    green: 13,
+    blue: 14,
+};
+
+impl FromStr for Round {
+    type Err = Error;
+
+    fn from_str(data: &str) -> Result<Self, Self::Err> {
+        let mut red = 0;
+        let mut green = 0;
+        let mut blue = 0;
+
+        let split_parts: Vec<&str> = data.split(",").map(|s| s.trim()).collect();
+
+        for part in split_parts {
+            let (number, colour) = part.split_once(" ").unwrap();
+            let qty = number.parse::<i32>().unwrap();
+
+            match colour {
+                "red" => red += qty,
+                "green" => green += qty,
+                "blue" => blue += qty,
+                _ => return Err(Error::NoColourFound),
+            };
         }
+        Ok(Self {
+            red: red,
+            green: green,
+            blue: blue,
+        })
     }
 }
 
+impl Round {
+    fn within_max(&self) -> bool {
+        self.red <= MAX_ROUND.red && self.green <= MAX_ROUND.green && self.blue <= MAX_ROUND.blue
+    }
+}
+
+#[derive(Debug)]
 struct Game {
     score: i32,
-    rounds: Vec<i32>,
+    rounds: Vec<Round>,
 }
 
 impl Game {
@@ -63,14 +64,28 @@ impl Game {
         let (name, all_rounds) = game_row.split_once(":").unwrap();
 
         let (_, score) = name.split_once(" ").unwrap();
-        let rounds: Vec<&str> = all_rounds.split(";").map(|n| n.trim()).collect();
+        let rounds: Vec<Round> = all_rounds
+            .split(";")
+            .map(|n| Round::from_str(n).unwrap())
+            .collect();
 
-        println!("{:?}", rounds);
         Self {
-            score: 10,
-            rounds: vec![1, 2, 3],
+            score: score.parse::<i32>().unwrap(),
+            rounds: rounds,
         }
     }
+
+    fn check_rounds(&self) -> bool {
+        self.rounds.iter().all(|round| round.within_max())
+    }
+}
+
+fn sum_winning_games(games: Vec<Game>) -> i32 {
+    games
+        .iter()
+        .filter(|&game| game.check_rounds())
+        .map(|game| game.score)
+        .sum()
 }
 
 fn read_input(filename: &str) -> Vec<String> {
@@ -82,4 +97,6 @@ fn read_input(filename: &str) -> Vec<String> {
 fn main() {
     let data = read_input("part_1.txt");
     let new_data: Vec<Game> = data.iter().map(|n| Game::new(&n)).collect();
+    let part_1 = sum_winning_games(new_data);
+    println!("{:?}", part_1);
 }
