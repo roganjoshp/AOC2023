afba6ac19699ed9450aab96b9befcaa58a4e179d
Josh Pilkington <j.pilkington@hotmail.com>
2024-02-08 14:16:20 +0000

    Finished day 3

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -1,19 +1,18 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 
-const NEIGHBOURHOOD: [[i32; 2]; 9] = [
+const NEIGHBOURHOOD: [[i32; 2]; 8] = [
     [-1, -1],
     [-1, 0],
     [-1, 1],
     [0, -1],
-    [0, 0], // pointlesss
     [0, 1],
     [1, -1],
     [1, 0],
     [1, 1],
 ];
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
 struct Digit {
     i: i32,
     j: i32,
@@ -34,8 +33,9 @@ impl Digit {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Hash, PartialEq, Eq, Clone)]
 struct Number {
+    id: i32,
     value: i32,
     digit_coords: Vec<Digit>,
 }
@@ -48,23 +48,27 @@ impl Number {
     }
 }
 
-fn build_maps(data: Vec<Vec<char>>) -> (Vec<Number>, HashSet<(i32, i32)>) {
+fn build_maps(data: Vec<Vec<char>>) -> (Vec<Number>, HashSet<(i32, i32)>, HashSet<(i32, i32)>) {
     let mut seen_chars: HashSet<(i32, i32)> = HashSet::new();
     let mut numbers: Vec<Number> = Vec::new();
+    let mut star_map: HashSet<(i32, i32)> = HashSet::new();
 
     let mut number_started: bool = false;
     let mut str_number = String::new();
     let mut digits: Vec<Digit> = Vec::new();
+    let mut seen_numbers = 0;
 
     for (i, row) in data.iter().enumerate() {
         if number_started {
             numbers.push(Number {
+                id: seen_numbers,
                 value: str_number.parse::<i32>().unwrap(),
                 digit_coords: digits.clone(),
             });
             number_started = false;
             str_number.clear();
             digits.clear();
+            seen_numbers += 1;
         }
         for (j, col_value) in row.iter().enumerate() {
             if col_value.is_ascii_digit() {
@@ -74,20 +78,60 @@ fn build_maps(data: Vec<Vec<char>>) -> (Vec<Number>, HashSet<(i32, i32)>) {
             } else {
                 if number_started {
                     numbers.push(Number {
+                        id: seen_numbers,
                         value: str_number.parse::<i32>().unwrap(),
                         digit_coords: digits.clone(),
                     });
                     number_started = false;
                     str_number.clear();
                     digits.clear();
+                    seen_numbers += 1;
                 }
                 if *col_value != '.' {
                     seen_chars.insert((i as i32, j as i32));
                 }
+                if *col_value == '*' {
+                    star_map.insert((i as i32, j as i32));
+                }
             }
         }
     }
-    (numbers, seen_chars)
+    (numbers, seen_chars, star_map)
+}
+
+fn build_inverted_number_map(numbers: &Vec<Number>) -> HashMap<(i32, i32), Number> {
+    let mut inverted_map: HashMap<(i32, i32), Number> = HashMap::new();
+    for number in numbers {
+        for digit in &number.digit_coords {
+            inverted_map.insert((digit.i, digit.j), number.clone());
+        }
+    }
+    inverted_map
+}
+
+fn find_gears(star_map: HashSet<(i32, i32)>, number_map: HashMap<(i32, i32), Number>) -> i32 {
+    let mut seen_numbers: HashSet<&Number> = HashSet::new();
+    let mut total = 0;
+
+    for star in star_map {
+        for neighbour in NEIGHBOURHOOD {
+            if number_map.contains_key(&(star.0 + neighbour[0], star.1 + neighbour[1])) {
+                let number = number_map
+                    .get(&(star.0 + neighbour[0], star.1 + neighbour[1]))
+                    .unwrap();
+                seen_numbers.insert(number);
+            }
+        }
+        if seen_numbers.len() == 2 {
+            let mut inner_total = 1;
+            for &number in &seen_numbers {
+                inner_total = inner_total * number.value;
+            }
+            total += inner_total
+        }
+        seen_numbers.clear();
+    }
+    total
 }
 
 fn read_input(filename: &str) -> Vec<Vec<char>> {
@@ -97,10 +141,9 @@ fn read_input(filename: &str) -> Vec<Vec<char>> {
 }
 
 fn main() {
-    let input = read_input("part_1.txt");
+    let input = read_input("test_file.txt");
 
-    let (numbers, symbol_indices) = build_maps(input);
-    println!("{}", numbers.len());
+    let (numbers, symbol_indices, star_map) = build_maps(input);
 
     let part_1: i32 = numbers
         .iter()
@@ -108,4 +151,8 @@ fn main() {
         .map(|x| x.value)
         .sum();
     println!("{}", part_1);
+
+    let inverted_map = build_inverted_number_map(&numbers);
+    let part_2 = find_gears(star_map, inverted_map);
+    println!("{}", part_2);
 }
